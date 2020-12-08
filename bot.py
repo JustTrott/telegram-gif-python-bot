5c695866a87a1498ecaf29961c8721de82a3c019
JustTrott <greenvisp@gmail.com>
2020-12-08 23:48:47 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,12 +23,12 @@ def send_a_gif(message):
     a_gif = 'CgACAgIAAxkBAAMEX8-j-W7aYYu6cCROigHjTibvhW4AAiwKAAIsiClILv-rgL5Bb1oeBA'
     bot.send_animation(message.chat.id, a_gif, reply_to_message_id=message.message_id)
 
-@bot.message_handler(func=lambda message: message.text is not None and (message.text.lower() == 'да' or message.text.lower() == 'нет'))
-def send_stupid_answer(message):
-    if message.text.lower() == 'нет':
-        bot.reply_to(message, "да")
-    elif message.text.lower() == 'да':
-        bot.reply_to(message, "нет")
+#@bot.message_handler(func=lambda message: message.text is not None and (message.text.lower() == 'да' or message.text.lower() == 'нет'))
+#def send_stupid_answer(message):
+#    if message.text.lower() == 'нет':
+#        bot.reply_to(message, "да")
+#    elif message.text.lower() == 'да':
+#        bot.reply_to(message, "нет")
 
 if __name__ == '__main__':
     print("Application has been started.")
